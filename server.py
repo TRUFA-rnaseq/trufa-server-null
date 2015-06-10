b59fa868ae7261d528f73f4b98e2f731d639e78e
Luis Cabellos <cabellos@ifca.unican.es>
2015-06-10 11:40:45 +0200

    apply pep8 style to server.py

--- server.py
+++ server.py
@@ -1,64 +1,68 @@
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 import web
 import json
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 urls = (
     '/jobs', 'Jobs',
     '/jobs/(.*)', 'Job'
 )
 
-app = web.application( urls, globals() )
+app = web.application(urls, globals())
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 class Jobs:
-    def GET( self ):
+    def GET(self):
         request = {}
         try:
-            request = json.loads( web.data() )
+            request = json.loads(web.data())
         except ValueError:
             print "error : decoding json request"
 
         print request
 
-        ret = {'ok': False, 'errormsg': "Not Implemented" }
+        ret = {'ok': False, 'errormsg': "Not Implemented"}
         web.header('Content-Type', 'applicacion/json')
         return json.dumps(ret)
 
-    def PUT( self ):
+    def PUT(self):
         request = {}
         try:
-            request = json.loads( web.data() )
+            request = json.loads(web.data())
         except ValueError:
             print "error : decoding json request"
 
         print request
 
-        ret = {'ok': False, 'errormsg': "Not Implemented" }
+        ret = {'ok': False, 'errormsg': "Not Implemented"}
         web.header('Content-Type', 'applicacion/json')
         return json.dumps(ret)
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 class Job:
-    def GET( self, job ):
-        ret = {'ok': False, 'errormsg': "Not Implemented" }
+    def GET(self, job):
+        ret = {'ok': False, 'errormsg': "Not Implemented"}
         web.header('Content-Type', 'applicacion/json')
-        return json.dumps(ret)        
+        return json.dumps(ret)
 
-    def POST( self, job ):
+    def POST(self, job):
         request = {}
         try:
-            request = json.loads( web.data() )
+            request = json.loads(web.data())
         except ValueError:
             print "error : decoding json request"
 
         print request
-        ret = {'ok': False, 'errormsg': "Not Implemented" }
+        ret = {'ok': False, 'errormsg': "Not Implemented"}
         web.header('Content-Type', 'applicacion/json')
-        return json.dumps(ret)        
-        
-#-------------------------------------------------------------------------------
+        return json.dumps(ret)
+
+
+# ------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run()
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
